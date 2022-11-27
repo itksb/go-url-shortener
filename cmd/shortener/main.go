278d5fc1c24f80458c9924003d22da43339f4c39
ksb <ksb@em70.ru>
2022-11-28 03:48:34 +0700

    itertaion 5

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"github.com/itksb/go-url-shortener/internal/app"
 	"github.com/itksb/go-url-shortener/internal/config"
 	"log"
+	"os"
 )
 
 func main() {
@@ -11,9 +13,33 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	useOsEnv(&cfg)
+
 	app, err := app.NewApp(cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Fatal(app.Run())
+}
+
+func useOsEnv(cfg *config.Config) {
+	host, ok := os.LookupEnv("SERVER_ADDRESS")
+	if ok {
+		cfg.AppHost = host
+	}
+
+	baseURL, ok := os.LookupEnv("BASE_URL")
+	if ok {
+		cfg.ShortBaseURL = baseURL
+	}
+
+	appPortStr, ok := os.LookupEnv("PORT")
+	intValue := 8080
+	_, err := fmt.Sscan(appPortStr, &intValue)
+	if err != nil {
+		log.Panic("PORT value is invalid")
+	}
+	if ok {
+		cfg.AppPort = intValue
+	}
 }
