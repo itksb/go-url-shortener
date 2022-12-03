c79150e6578a3f1afc41c25dddf84b998a189837
ksb <ksb@em70.ru>
2022-12-03 19:25:52 +0700

    Refactoring. Moves configuration to the config package

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"github.com/itksb/go-url-shortener/internal/app"
 	"github.com/itksb/go-url-shortener/internal/config"
 	"log"
-	"os"
-	"strings"
 )
 
 func main() {
@@ -15,8 +11,8 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	useOsEnv(&cfg)
-	useFlags(&cfg)
+	cfg.UseOsEnv()
+	cfg.UseFlags()
 
 	app, err := app.NewApp(cfg)
 	if err != nil {
@@ -24,71 +20,4 @@ func main() {
 	}
 	defer app.Close()
 	log.Fatal(app.Run())
-}
-
-func useOsEnv(cfg *config.Config) {
-	host, ok := os.LookupEnv("SERVER_ADDRESS")
-	if ok {
-		addr := strings.SplitN(host, ":", 2)
-		if len(addr) == 2 {
-			cfg.AppHost = addr[0]
-			intValue := 8080
-			_, err := fmt.Sscan(addr[1], &intValue)
-			if err != nil {
-				log.Panic("PORT value is invalid")
-			}
-			if ok {
-				cfg.AppPort = intValue
-			}
-		} else {
-			cfg.AppHost = host
-		}
-	}
-
-	baseURL, ok := os.LookupEnv("BASE_URL")
-	if ok {
-		cfg.ShortBaseURL = baseURL
-	}
-
-	appPortStr, ok := os.LookupEnv("PORT")
-	if ok {
-		intValue := 8080
-		_, err := fmt.Sscan(appPortStr, &intValue)
-		if err != nil {
-			log.Panic("PORT value is invalid")
-		}
-		if ok {
-			cfg.AppPort = intValue
-		}
-	}
-
-	fileStoragePath, ok := os.LookupEnv("FILE_STORAGE_PATH")
-	if ok {
-		cfg.FileStoragePath = fileStoragePath
-	}
-
-}
-
-func useFlags(cfg *config.Config) {
-	appHost := flag.String("a", cfg.AppHost, "SERVER_ADDRESS")
-	shortBaseURL := flag.String("b", cfg.ShortBaseURL, "BASE_URL")
-	fileStoragePath := flag.String("f", cfg.FileStoragePath, "FILE_STORAGE_PATH")
-	flag.Parse()
-
-	addr := strings.SplitN(*appHost, ":", 2)
-	if len(addr) == 2 {
-		cfg.AppHost = addr[0]
-		intValue := 8080
-		_, err := fmt.Sscan(addr[1], &intValue)
-		if err != nil {
-			log.Panic("PORT value is invalid")
-		}
-		cfg.AppPort = intValue
-
-	} else {
-		cfg.AppHost = *appHost
-	}
-
-	cfg.ShortBaseURL = *shortBaseURL
-	cfg.FileStoragePath = *fileStoragePath
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,13 @@
 package config
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
 type (
 	// Config - Application configuration
 	Config struct {
@@ -20,4 +28,73 @@ func NewConfig() (Config, error) {
 		FileStoragePath: "",
 	}
 	return cfg, nil
+}
+
+// UseOsEnv - apply environment variables
+func (cfg *Config) UseOsEnv() {
+	host, ok := os.LookupEnv("SERVER_ADDRESS")
+	if ok {
+		addr := strings.SplitN(host, ":", 2)
+		if len(addr) == 2 {
+			cfg.AppHost = addr[0]
+			intValue := 8080
+			_, err := fmt.Sscan(addr[1], &intValue)
+			if err != nil {
+				log.Panic("PORT value is invalid")
+			}
+			if ok {
+				cfg.AppPort = intValue
+			}
+		} else {
+			cfg.AppHost = host
+		}
+	}
+
+	baseURL, ok := os.LookupEnv("BASE_URL")
+	if ok {
+		cfg.ShortBaseURL = baseURL
+	}
+
+	appPortStr, ok := os.LookupEnv("PORT")
+	if ok {
+		intValue := 8080
+		_, err := fmt.Sscan(appPortStr, &intValue)
+		if err != nil {
+			log.Panic("PORT value is invalid")
+		}
+		if ok {
+			cfg.AppPort = intValue
+		}
+	}
+
+	fileStoragePath, ok := os.LookupEnv("FILE_STORAGE_PATH")
+	if ok {
+		cfg.FileStoragePath = fileStoragePath
+	}
+
+}
+
+// UseFlags - scan flags
+func (cfg *Config) UseFlags() {
+	appHost := flag.String("a", cfg.AppHost, "SERVER_ADDRESS")
+	shortBaseURL := flag.String("b", cfg.ShortBaseURL, "BASE_URL")
+	fileStoragePath := flag.String("f", cfg.FileStoragePath, "FILE_STORAGE_PATH")
+	flag.Parse()
+
+	addr := strings.SplitN(*appHost, ":", 2)
+	if len(addr) == 2 {
+		cfg.AppHost = addr[0]
+		intValue := 8080
+		_, err := fmt.Sscan(addr[1], &intValue)
+		if err != nil {
+			log.Panic("PORT value is invalid")
+		}
+		cfg.AppPort = intValue
+
+	} else {
+		cfg.AppHost = *appHost
+	}
+
+	cfg.ShortBaseURL = *shortBaseURL
+	cfg.FileStoragePath = *fileStoragePath
 }
