e0e18927a3b1562e394632a566aa6ea47e8446bf
ksb <ksb@em70.ru>
2022-12-18 20:14:42 +0600

    ui

--- web/classic/js/main.js
+++ web/classic/js/main.js
@@ -3,7 +3,7 @@ import App from "./app.js"
 import {ShortenApi} from "./api.js";
 
 
-const config = NewConfig({baseUrl: "//localhost:8080"})
+const config = NewConfig({baseUrl: "//localhost:8090"})
 const shortenApi = new ShortenApi(config,  fetch.bind(window))
 const app = new App({config, shortenApi})
 app.run()
