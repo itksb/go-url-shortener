e8d98837cfdbf7f1523b2c654ea73b496d02a8fa
Sergey Kochetkov <s.kochetkov@it.ecommpay.com>
2023-04-13 10:40:56 +0600

    iter19: ldflags

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"github.com/itksb/go-url-shortener/internal/app"
 	"github.com/itksb/go-url-shortener/internal/config"
 	"log"
 )
 
+// go run -ldflags "-X main.Version=v1.1.1 \
+// -X 'main.buildVersion=1.1.1'" ./cmd/shortener/main.go
+var (
+	buildVersion string = "N/A"
+	buildDate    string = "N/A"
+	buildCommit  string = "N/A"
+)
+
 func main() {
+
+	fmt.Printf(
+		"Build version: %s\nBuild date: %s\nBuild commit: %s\n",
+		buildVersion,
+		buildDate,
+		buildCommit,
+	)
+
 	cfg, err := config.NewConfig()
 	if err != nil {
 		log.Fatal(err)
